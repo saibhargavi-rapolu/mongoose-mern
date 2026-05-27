3b9a66eebf64048f7c739a536ace426a43931c0e
bhargavi-rapolu <saibhargavi.rapolu5@gmail.com>
2026-05-27 13:26:56 +0530

    files added

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/studentDB")
+mongoose.connect("mongodb://localhost:27017/StudentDB")
 .then(() => {
     console.log("MongoDB Connected");
 })
